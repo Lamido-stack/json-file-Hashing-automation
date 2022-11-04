634c6cd170177225359416940a4d5fa30bcef5c1
Lamido <uklamido@gmail.com>
2022-11-04 02:21:53 +0100

    fixed some errors

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Creating a programme to generate a json file and a sha256 and output.csv
 
 #required
---import pandas as pd
+--import csv
+--import json
 --import hashlib
 --from csv import writer
 --from csv import reader
@@ -13,6 +14,10 @@ Creating a programme to generate a json file and a sha256 and output.csv
 How it works::
 it runs on the terminal
 -returns a prompt asking the user for the file path for the .CSV file
+!!!example of how the file path might look : ./New_output/nft.CSV
 ones the user inputs the path it returns
             a new .CSV file with new rows containing the hashed key of the specific json file for that row
             json files of each rows
+***WHAT THE SCRIPT DOES
+-it creates a json file for every line in the .csv file and stores them
+-it hashes the json file of every line and appends the value beside the line in a new .csv file
--- tasktwo.py
+++ tasktwo.py
@@ -1,61 +1,50 @@
-import pandas as pd
 import hashlib
+import csv
+import json
 from csv import writer
 from csv import reader
+
+
 #get's the file paths
 datapath =input("paste your file path here: ")
 outpath = datapath[0:-4]
 
-#reads through the csv file to be processed
-data = pd.read_csv(datapath)
-
-
-
-serialNum = data.SerialNumber
-fileName = data.Filename
-# description = data.Description
-# gender = data.Gender
-uuid = data.UUID
-
-#container to store the contents of the csv file after
-container ={}
-
-x = 0
-
-while x < len(fileName):
-    container[fileName[x]]=[
-        {"SerialNumber ":serialNum[x],
-        "Filename ":fileName[x],
-        "UUID ":uuid[x],}
-        ]
-    df = pd.DataFrame(container)
-    df.to_json(outpath, indent=4)
-    x = x+1
-
-#converts every row into a json file, hashes each file 
+#loops through the entire .CSV file and rtrieve every data
+container = {}
 hashes=['HASH']
-for i in container:
-    filePath = outpath+'.'+i+'.json'
-    df = pd.DataFrame(container[i])
-    df.to_json(filePath, indent=4)
-    with open(filePath,"rb") as f:
-        bytes = f.read() # read entire file as bytes
-        readable_hash = hashlib.sha256(bytes).hexdigest();
-        hashes.append(readable_hash)
-        
+with open(datapath, encoding='utf-8') as doc:
+    data = csv.DictReader(doc)
+    for i in data:
+        rows = i['Name']
+        container[rows] = i
+        #converts every row into a json file
+        miniContainer = {}
+        miniContainer[rows] = i 
+        with open(rows+'.json', 'w',  encoding='utf-8') as minidoc:
+            minidoc.write(json.dumps(miniContainer, indent=4))
+            # hashes each file and stores the value in the variable "hashes"
+            with open(rows+'.json',"rb") as f:
+                bytes = f.read() # read entire file as bytes
+                readable_hash = hashlib.sha256(bytes).hexdigest();
+                hashes.append(readable_hash)
+
+# creates a .json copy of the csv file
+with open(outpath+'.json', 'w',  encoding='utf-8') as outDoc:
+        outDoc.write(json.dumps(container, indent=4))
+
+    
 # Open the input_file in read mode and output_file in write mode, creates a new column and merges every row with its hashed value
-y=0
-while y < len(fileName):
-    with open(datapath, 'r') as read_obj, \
-            open(outpath+'.output'+'.csv', 'w', newline='') as write_obj:
-        # Create a csv.reader object from the input file object
-        csv_reader = reader(read_obj)
+y = 0
+with open(datapath, 'r') as read_obj, \
+        open(outpath+'.output'+'.csv', 'w', newline='') as write_obj:
+ # Create a csv.reader object from the input file object
+    csv_reader = reader(read_obj)
         # Create a csv.writer object from the output file object
-        csv_writer = writer(write_obj)
+    csv_writer = writer(write_obj)
         # Read each row of the input csv file as list
-        for row in csv_reader:
+    for row in csv_reader:
                 # Append the default text in the row / list
-            row.append(hashes[y])
+        row.append(hashes[y])
                 # Add the updated row / list to the output file
-            csv_writer.writerow(row)
-            y += 1
+        csv_writer.writerow(row)
+        y += 1
